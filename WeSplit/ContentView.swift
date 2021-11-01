6165efb7af97f17e22a2c20f127433972207864c
Agbesi Amenyo <56748821+agbesi-amenyo@users.noreply.github.com>
2021-11-01 02:10:35 +0100

    calculations made-end

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -10,10 +10,24 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var checkAmount = 0.0
-    @State private var numberOfPeople = 0
+    @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
     
     let tipPercentages = [10,15,20,25,0]
+    
+    var totalPerPerson : Double {
+        let peopleCount = Double (numberOfPeople + 2)
+        
+        let tipSelection = Double (tipPercentage )
+        let tipValue = checkAmount/100 * tipSelection
+        
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal/peopleCount
+
+        
+        return amountPerPerson
+        
+    }
     
      
     var body: some View {
@@ -28,23 +42,15 @@ struct ContentView: View {
                     } else {
                         // Fallback on earlier versions
                     }
-                }
-                
-                Section {
-                    if #available(iOS 15.0, *) {
-                        Text(checkAmount, format: .currency(code: Locale.current.currencyCode ?? "EUR"))
-                    } else {
-                        // Fallback on earlier versions
-                    }
                     
                     Picker("Number of people", selection:$numberOfPeople){
                         ForEach(2..<100){
                             Text("\($0) people")
                         }
                     }
-                    
-                    
                 }
+                
+             
                 
                 Section {
                     Picker("Tip percentage", selection: $tipPercentage){
@@ -58,6 +64,19 @@ struct ContentView: View {
                     }.pickerStyle(.segmented)
                 } header: {
                     Text("How much tip do you want to leave? ")
+                }
+                
+                
+                Section {
+                    if #available(iOS 15.0, *) {
+                        Text(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "EUR"))
+                    } else {
+                        // Fallback on earlier versions
+                    }
+                  
+                    
+                }header: {
+                    Text("Here is how much you will pay :")
                 }
                 
             }
